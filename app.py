a87395d46daf0c713465680f6490f28bd057b897
Ashish <webashu@gmail.com>
2023-02-03 12:35:39 +0530

    changed the port

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def homepage():
     if request.method == "GET":
         return jsonify({"message": "Hello User!"})
 
-PORT = int(os.environ.get("PORT", 8080))
+# PORT = int(os.environ.get("PORT", 8080))
+PORT = 8080
 if __name__ == '__main__':
     app.run(threaded=True,host='0.0.0.0',port=PORT)
