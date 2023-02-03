a4e982c23c5109f7bba7c79c07aff8c0f397d3cd
Ashish <webashu@gmail.com>
2023-02-03 13:16:52 +0530

    changed display message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def homepage():
     if request.method == "GET":
-        return jsonify({"message": "Hello User!"})
+        return jsonify({"message": "Hello Ashish welcome to CloudRun!"})
 
 # PORT = int(os.environ.get("PORT", 8080))
 PORT = 8080
